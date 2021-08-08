7e99475f644cc1a906005330844626dc94f39ebd
Masahiro Nakao <masahiro.nakao@riken.jp>
2021-08-08 10:42:42 +0900

    minor fix

--- misc/histgram.c
+++ misc/histgram.c
@@ -3,54 +3,59 @@
 #include <limits.h>
 #include "orp.h"
 
-static int search_max(const int switches, const int *s_degree)
-{
-  int max = s_degree[0];
-  for(int i=1;i<switches;i++){
-    if(s_degree[i] > max)
-      max = s_degree[i];
-  }
-  return max;
-}
-
-static int search_min(const int switches, const int *s_degree)
-{
-  int min = s_degree[0];
-  for(int i=1;i<switches;i++){
-    if(s_degree[i] < min)
-      min = s_degree[i];
-  }
-  return min;
-}
-
 int main(int argc, char *argv[])
 {
+  int hosts, switches, radix, lines, diameter, low_diameter;
+  long sum;
+  double ASPL, low_ASPL;
+  
   if(argc != 2){
     fprintf(stderr, "%s <edgefile>\n", argv[0]);
     exit(1);
   }
-
-  int hosts, switches, radix, lines;
   ORP_Read_property(argv[1], &hosts, &switches, &radix, &lines);
-  printf("(h, r, s) = (%d, %d, %d)\n", hosts, radix, switches);
-
+  
   int *h_degree  = malloc(sizeof(int) * switches);
   int *s_degree  = malloc(sizeof(int) * switches);
   int (*edge)[2] = malloc(sizeof(int) * lines * 2);
+  int (*adjacency)[radix] = malloc(sizeof(int) * switches * radix);
+  
   ORP_Read_edge(argv[1], edge);
   ORP_Set_degrees(hosts, switches, lines, edge, h_degree, s_degree);
+  ORP_Conv_edge2adjacency(hosts, switches, radix, lines, edge, adjacency);
+  
+  ORP_Init_aspl(hosts, switches, radix);
+  ORP_Set_aspl(h_degree, s_degree, adjacency, &diameter, &sum, &ASPL);
+  ORP_Finalize_aspl();
 
-  int max      = search_max(switches, s_degree);
-  int min      = search_min(switches, s_degree);
-  int elements = max - min + 1;
-  int *hist    = malloc(elements * sizeof(int));
-  for(int i=0;i<elements;i++) hist[i] = 0;
+  ORP_Set_lbounds(hosts, radix, &low_diameter, &low_ASPL);
+  printf("Hosts = %d, Switches %d, Radix = %d\n", hosts, switches, radix);
+  printf("Diameter     = %d\n", diameter);
+  printf("Diameter Gap = %d (%d - %d)\n", diameter - low_diameter, diameter, low_diameter);
+  printf("ASPL         = %.10f (%ld/%.0f)\n", ASPL, sum, (double)hosts*(hosts-1)/2);
+  printf("ASPL Gap     = %.10f (%.10f - %.10f)\n", ASPL - low_ASPL, ASPL, low_ASPL);
+  printf("--\n");
+  
+  int *hist = malloc((radix+1) * sizeof(int));
+  for(int i=0;i<radix+1;i++)
+    hist[i] = 0;
 
   for(int i=0;i<switches;i++)
-    hist[s_degree[i]-min]++;
+    hist[s_degree[i]]++;
 
-  for(int i=min;i<max+1;i++)
-    printf("%d : %d\n", i, hist[i-min]);
+  int s = 0;
+  printf("#Switches connected to switches : num\n");
+  for(int i=1;i<radix+1;i++){
+    s += hist[i];
+    printf("%d %d\n", i, hist[i]);
+  }
+  printf("---\n");
+  printf("SUM : %d\n", s);
+  
+  if(s != switches || hist[0] != 0){
+    printf("Error\n");
+    exit(1);
+  }
   
   return 0;
 }
