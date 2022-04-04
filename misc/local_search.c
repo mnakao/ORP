ea915fbb84cdcadfdf386656858e827fea23066e
Masahiro Nakao <masahiro.nakao@riken.jp>
2022-04-04 22:50:59 +0900

    minor fix

--- misc/local_search.c
+++ misc/local_search.c
@@ -1,22 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "orp.h"
+#define ERROR(...) do{fprintf(stderr,__VA_ARGS__); exit(1);}while(0)
+#define MAX_FILENAME_LENGTH 256
+#define NOT_DEFINED -1
+
+static void print_help(char *argv)
+{
+  fprintf(stderr, "%s -f input -o output [-G symmetries]\n", argv);
+  fprintf(stderr, "  -f : Input file\n");
+  fprintf(stderr, "  -o : Output file\n");
+  fprintf(stderr, "  -G : Numer of symmetries (Default: 1)\n");
+  exit(1);
+}
+
+static void set_args(const int argc, char **argv, char **infname, char **outfname, int *symmetries)
+{
+  int result;
+  while((result = getopt(argc,argv,"f:o:G:"))!=-1){
+    switch(result){
+    case 'f':
+      if(strlen(optarg) > MAX_FILENAME_LENGTH)
+        ERROR("Input filename is long (%s).\n", optarg);
+      *infname = malloc(MAX_FILENAME_LENGTH);
+      strcpy(*infname, optarg);
+      break;
+    case 'o':
+      if(strlen(optarg) > MAX_FILENAME_LENGTH)
+        ERROR("Output filename is long (%s).\n", optarg);
+      *outfname = malloc(MAX_FILENAME_LENGTH);
+      strcpy(*outfname, optarg);
+      break;
+    case 'G':
+      *symmetries = atoi(optarg);
+      if(*symmetries < 1)
+        ERROR("-G value >= 1\n");
+      break;
+    default:
+      print_help(argv[0]);
+    }
+  }
+}
+
+static int tmp[2][2];
+static void backup(const int i, const int j, const int radix, const int (*adjacency)[radix])
+{
+  tmp[0][0] = i; tmp[0][1] = tmp[1][0] = j;
+  
+}
+
+static int search_index(const int v, const int target, const int exclusion, const int *s_degree,
+                        const int radix, const int (*adjacency)[radix])
+{
+  if(v == target){
+    for(int i=0;i<s_degree[v];i++){
+      if(adjacency[v][i] == target && i != exclusion){
+        return i;
+      }
+    }
+  }
+  else{
+    for(int i=0;i<s_degree[v];i++){
+      if(adjacency[v][i] == target){
+        return i;
+      }
+    }
+  }
+
+  ERROR("Something Wrong (id=2)\n");
+  return -1; // dummy
+}
 
 int main(int argc, char **argv)
 {
-  int hosts, switches, radix, lines, diameter = -1, low_diameter = -1, new_diameter = -1;
-  long sum = -1, new_sum = -1;
-  double ASPL = -1, low_ASPL = -1, new_ASPL = -1;
+  char *infname = NULL, *outfname = NULL;
+  int hosts = NOT_DEFINED, switches = NOT_DEFINED, radix = NOT_DEFINED, lines = NOT_DEFINED;
+  int diameter = NOT_DEFINED, low_diameter = NOT_DEFINED, new_diameter = NOT_DEFINED, symmetries = 1;
+  long sum = NOT_DEFINED, new_sum = NOT_DEFINED;
+  double ASPL = NOT_DEFINED, low_ASPL = NOT_DEFINED, new_ASPL = NOT_DEFINED;
+  bool flag = false;
 
-  if(argc != 2){
-    fprintf(stderr, "Please add an edge file. \"%s xxx.edges\"\n", argv[0]);
-    exit(1);
-  }
-    
-  ORP_Read_property(argv[1], &hosts, &switches, &radix, &lines);
+  set_args(argc, argv, &infname, &outfname, &symmetries);
+  if(infname == NULL || outfname == NULL)
+    ERROR("./%s -f input -o output\n", argv[0]);
+  
+  ORP_Read_property(infname, &hosts, &switches, &radix, &lines);
+  printf("Hosts = %d, Switches = %d, Radix = %d\n", hosts, switches, radix);
+
   int (*edge)[2] = malloc(sizeof(int)*lines*2); // int edge[lines][2];
-  ORP_Read_edge(argv[1], edge);
+  ORP_Read_edge(infname, edge);
   
   int *s_degree = malloc(sizeof(int) * switches);
   int *h_degree = malloc(sizeof(int) * switches);
@@ -28,34 +102,38 @@ int main(int argc, char **argv)
   ORP_Init_aspl(hosts, switches, radix);
   ORP_Set_aspl(h_degree, s_degree, adjacency, &diameter, &sum, &ASPL);
   ORP_Set_lbounds(hosts, radix, &low_diameter, &low_ASPL);
-  //  printf("%f\n", ASPL - low_ASPL);
-
-  int (*new_edge)[2] = malloc(sizeof(int) * lines * 2);
-  int *new_s_degree  = malloc(sizeof(int) * switches);
-  int *new_h_degree  = malloc(sizeof(int) * switches);
-  int new_lines      = lines - 1;
-  char fname[100];
-  for(int i=hosts;i<lines;i++){
-    memcpy(new_edge, edge, sizeof(int) * lines * 2);
-    if(new_edge[i][0] < hosts || new_edge[i][1] < hosts) exit(1);
-    new_edge[i][0] = new_edge[lines-1][0];
-    new_edge[i][1] = new_edge[lines-1][1];
-    ORP_Set_degrees(hosts, switches, new_lines, new_edge, new_h_degree, new_s_degree);
-    ORP_Conv_edge2adjacency(hosts, switches, radix, new_lines, new_edge, adjacency);
-    ORP_Set_aspl(new_h_degree, new_s_degree, adjacency, &new_diameter, &new_sum, &new_ASPL);
-    if(new_ASPL <= ASPL){
-      sprintf(fname, "%d.edges", i);
-      ORP_Write_edge(hosts, switches, radix, new_lines, new_edge, fname);
-      printf("Found\n");
-      exit(0);
+  
+  printf("Diameter     = %d\n", diameter);
+  printf("Diameter Gap = %d (%d - %d)\n", diameter - low_diameter, diameter, low_diameter);
+  printf("ASPL         = %.10f (%ld/%.0f)\n", ASPL, sum, (double)hosts*(hosts-1)/2);
+  printf("ASPL Gap     = %.10f (%.10f - %.10f)\n", ASPL - low_ASPL, ASPL, low_ASPL);
+  printf("--\n");
+  for(int u=0;u<switches/symmetries;u++){
+    for(int u_d=0;u_d<s_degree[u];u_d++){
+      int v   = adjacency[u][u_d];
+      int v_d = search_index(v, u, u_d, s_degree, radix, adjacency);
+      adjacency[u][u_d] = adjacency[u][s_degree[u]-1];
+      s_degree[u]--;
+      adjacency[v][v_d] = adjacency[v][s_degree[v]-1];
+      s_degree[v]--;
+      ORP_Set_aspl(h_degree, s_degree, adjacency, &new_diameter, &new_sum, &new_ASPL);
+      if((new_ASPL <= ASPL && new_diameter <= diameter) || new_diameter < diameter){
+        flag = true;
+        printf("Delete : %d -- %d\n", u+hosts, v+hosts);
+        printf(" Diameter Gap = %d (%d - %d)\n", new_diameter - low_diameter, new_diameter, low_diameter);
+        printf(" ASPL Gap     = %.10f (%.10f - %.10f)\n", new_ASPL - low_ASPL, new_ASPL, low_ASPL);
+      }
+      
+      adjacency[u][u_d] = v; s_degree[u]++;
+      adjacency[v][v_d] = u; s_degree[v]++;
     }
   }
-  
   ORP_Finalize_aspl();
- 
-  free(adjacency);
-  free(h_degree);
-  free(s_degree);
-  free(edge);
+  
+  if(!flag)
+    printf("Not found.\n");
+  
+  free(adjacency); free(h_degree);
+  free(s_degree);  free(edge);
   return 0;
 }
